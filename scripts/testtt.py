8cdab18e62dd74a2212f9ef29a1da911791d3fb0
Smart Idima <e187357#exelonds.com>
2025-09-22 20:28:38 -0500

    chore: Remove obsolete event CSV files and MPS test script

--- scripts/testtt.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.backends.mps.is_available())      # True if MPS hardware detected
-print(torch.backends.mps.is_built())          # Should be True as well
